42983010bc42d63023d528a8f89ff94b46d9e0a2
Sven Bohm <bohms@msu.edu>
2014-04-27 04:13:58 -0400

    signature byte calc

--- pakbus.go
+++ pakbus.go
@@ -3,27 +3,43 @@ package pakbus
 import "strings"
 
 func quoteSyncByte(pkt string) string {
-  return strings.Replace(pkt, "\xbc", "\xbc\xdc", -1)
+	return strings.Replace(pkt, "\xbc", "\xbc\xdc", -1)
 }
 
 func quoteQuoteByte(pkt string) string {
-  return strings.Replace(pkt, "\xbd", "\xbc\xdd", -1)
+	return strings.Replace(pkt, "\xbd", "\xbc\xdd", -1)
 }
 
 func Quote(pkt string) string {
-  output := quoteSyncByte(pkt)
-  return quoteQuoteByte(output)
+	output := quoteSyncByte(pkt)
+	return quoteQuoteByte(output)
 }
 
 func unquoteSyncByte(pkt string) string {
-  return strings.Replace(pkt, "\xbc\xdc", "\xbc", -1)
+	return strings.Replace(pkt, "\xbc\xdc", "\xbc", -1)
 }
 
 func unquoteQuoteByte(pkt string) string {
-  return strings.Replace(pkt, "\xbc\xdd", "\xbd", -1)
+	return strings.Replace(pkt, "\xbc\xdd", "\xbd", -1)
 }
 
 func UnQuote(pkt string) string {
-  output := unquoteSyncByte(pkt)
-  return unquoteQuoteByte(output)
+	output := unquoteQuoteByte(pkt)
+	return unquoteSyncByte(output)
+}
+
+func CalcSigForByte(value byte, seed uint16) uint16 {
+	sig := (seed << 1) & 0x1ff
+	if sig >= 0x100 {
+		sig += 1
+	}
+	return (((sig + (seed >> 8) + uint16(value)) & 0xff) | (seed << 8)) & 0xffff
+}
+
+func CalcSigFor(buffer []byte, seed uint16) uint16 {
+	sig := seed
+	for _, value := range buffer {
+		sig = CalcSigForByte(value, sig)
+	}
+	return sig
 }
--- pakbus_test.go
+++ pakbus_test.go
@@ -3,43 +3,57 @@ package pakbus
 import "testing"
 
 func TestQuotePlain(t *testing.T) {
-  const in, out = "trim", "trim"
-  if x := Quote(in); x != out{
-		t.Errorf("Quote(%s) expected %s got %s", in, out, x )
+	const in, out = "trim", "trim"
+	if x := Quote(in); x != out {
+		t.Errorf("Quote(%s) expected %s got %s", in, out, x)
 	}
 }
 
 func TestQuoteSyncByte(t *testing.T) {
-  const in, out = "\xbcsync", "\xbc\xdcsync"
-  if x := Quote(in); x != out {
-    t.Errorf("Quote(%x) expected %x got %x", in, out, x )
-  }
-}
-
-func TestQuoteQuoteByte(t * testing.T) {
-  const in, out = "\xBDquote", "\xBC\xDDquote"
-  if x := Quote(in); x != out {
-    t.Errorf("Quote(%x) expected %x got %x", in, out, x )
-  }
-}
-
-func testUnquotePlain(t *testing.T) {
-  const in, out = "trim", "trim"
-  if x := UnQuote(in); x != out{
-		t.Errorf("UnQuote(%s) expected %s got %s", in, out, x )
+	const in, out = "\xbcsync", "\xbc\xdcsync"
+	if x := Quote(in); x != out {
+		t.Errorf("Quote(%x) expected %x got %x", in, out, x)
 	}
 }
 
-func testUnquoteSyncByte(t *testing.T) {
-  const in, out = "\xbc\xdcsync", "\xbcsync"
-  if x := UnQuote(in); x != out {
-    t.Errorf("Quote(%x) expected %x got %x", in, out, x )
-  }
+func TestQuoteQuoteByte(t *testing.T) {
+	const in, out = "\xBDquote", "\xBC\xDDquote"
+	if x := Quote(in); x != out {
+		t.Errorf("Quote(%x) expected %x got %x", in, out, x)
+	}
 }
 
-func testUnquoteQuoteByte(t *testing.T) {
-  const in, out = "\xBC\xDDquote", "\xBDquote"
-  if x := UnQuote(in); x != out {
-    t.Errorf("Quote(%x) expected %x got %x", in, out, x )
-  }
+func TestUnquotePlain(t *testing.T) {
+	const in, out = "trim", "trim"
+	if x := UnQuote(in); x != out {
+		t.Errorf("UnQuote(%s) expected %s got %s", in, out, x)
+	}
+}
+
+func TestUnquoteSyncByte(t *testing.T) {
+	const in, out = "\xbc\xdcsync", "\xbcsync"
+	if x := UnQuote(in); x != out {
+		t.Errorf("UnQuote(%x) expected %x got %x", in, out, x)
+	}
+}
+
+func TestUnquoteQuoteByte(t *testing.T) {
+	const in, out = "\xBC\xDDquote", "\xBDquote"
+	if x := UnQuote(in); x != out {
+		t.Errorf("UnQuote(%x) expected %x got %x", in, out, x)
+	}
+}
+
+func TestCalcSigForByte(t *testing.T) {
+	const in, out = 0xaa, 0xaaa9
+	if x := CalcSigForByte(in, 0xaaaa); x != out {
+		t.Errorf("CalcSigForByte(%x) expected %x got %x", in, out, x)
+	}
+}
+
+func TestCalcFor(t *testing.T) {
+	const in, out = "message", 0xbbdc
+	if x := CalcSigFor([]byte(in), 0xaaaa); x != out {
+		t.Errorf("CalcSigFor(%x) expected %x got %x", in, out, x)
+	}
 }
